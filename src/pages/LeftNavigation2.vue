14b70d02c6dd4fd96714a0f97c917fd7aa0c8a07
somisg <somis@gradfathersolutions.com>
2025-06-28 12:43:17 +0530

    accessids add

--- src/pages/LeftNavigation2.vue
+++ src/pages/LeftNavigation2.vue
@@ -8,49 +8,48 @@
             <li>
                 <span class="left-menu-text">MENU</span>
             </li>
-            <li>
+            <li v-if="accessid(421)">
                 <router-link to="/patientmanager">
                     <i class='bx bx-user-plus'></i>
                     <span class="link_name">Patient</span>
                 </router-link>
             </li>
-            <li>
+            <li  v-if="accessid(422)">
                 <router-link to="/institute">
                     <i class='bx bx-buildings'></i>
                     <span class="link_name">Institute</span>
                 </router-link>
             </li>
-            <li>
+            <li v-if="accessid(423)">
                 <router-link to="/hospitalmanager">
                     <i class='bx bx-plus-medical'></i>
                     <span class="link_name">Nursing Home</span>
                 </router-link>
             </li>
-            <li>
+            <li v-if="accessid(424)">
                 <router-link to="/studentmanager">
                     <i class='bx bx-user'></i>
                     <span class="link_name">Students</span>
                 </router-link>
             </li>
-            <li>
+            <li  v-if="accessid(425)">
                 <router-link to="/mobilizermanager">
                     <i class='bx bx-user'></i>
                     <span class="link_name">Mobilizer</span>
                 </router-link>
             </li>
-            <li>
-                <router-link to="/access">
-                    <i class='bx bx-gears'></i> 
-                    <span class="link_name">Permission</span>
-                </router-link>
-            </li>
-            <li>
+             <li v-if="accessid(426)">
                 <router-link to="/employee">
                     <i class="bx bx-user" ></i>
                     <span class="link_name">Employee</span>
                 </router-link>
             </li>
-
+            <li v-if="loggedinuser?.role>1">
+                <router-link to="/access">
+                    <i class='bx bx-setting'></i> 
+                    <span class="link_name">Permission</span>
+                </router-link>
+            </li>
             <li>
                 <router-link to="logout" @click="logout()">
                     <i class='bx bx-log-out-circle'></i>
